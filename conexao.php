7079aba8507b050ed6ee57dedbe52b1b211ee25e
PepeuAlex <50303742+PepeuAlex@users.noreply.github.com>
2020-06-23 20:31:25 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
-define('HOST', 'judo-app-001-271f2b5ac2');
-define('USUARIO', 'kappa');
-define('SENHA', 'Ped09ale');
+define('HOST', 'judo-app-001-b2b560715b');
+define('USUARIO', 'pepeu');
+define('SENHA', 'kappa');
 define('DB', 'login');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
